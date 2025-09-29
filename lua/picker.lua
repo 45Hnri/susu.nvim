10fb235726d56a3952a99e4932017e3f975dd005
45Hnri <mailhenrimal@web.de>
2025-09-29 02:06:44 +0200

    refactor: easier config merging

--- lua/picker.lua
+++ lua/picker.lua
@@ -5,31 +5,31 @@ local finders = require "telescope.finders"
 local conf = require("telescope.config").values
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
-local symbols = require"symbols"
+local symbols = require "symbols"
 
 M.get_symbols = function(opts)
-    opts = opts or {}
-    pickers.new(opts, {
-        prompt_title = "sub-/superscript symbols",
-        finder = finders.new_table {
-            results = symbols,
-            entry_maker = function(entry)
-                return {
-                    value = entry,
-                    display = entry[1].." "..entry[2].." "..entry[3],
-                    ordinal = entry[2]..entry[3],
-                }
-            end
-        },
-        sorter = conf.generic_sorter(opts),
-        attach_mappings = function(prompt_bufnr, map)
-            actions.select_default:replace(function()
-                actions.close(prompt_bufnr)
-                local selection = action_state.get_selected_entry()
-                vim.api.nvim_put({ selection.value[1] }, "", false, true)
-            end)
-            return true
-        end,
-    }):find()
+  opts = opts or {}
+  pickers.new(opts, {
+    prompt_title = "sub-/superscript symbols",
+    finder = finders.new_table {
+      results = symbols,
+      entry_maker = function(entry)
+        return {
+          value = entry,
+          display = entry[1] .. " " .. entry[2] .. " " .. entry[3],
+          ordinal = entry[2] .. entry[3],
+        }
+      end
+    },
+    sorter = conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        vim.api.nvim_put({ selection.value[1] }, "", false, true)
+      end)
+      return true
+    end,
+  }):find()
 end
 return M
--- lua/susu.lua
+++ lua/susu.lua
@@ -10,59 +10,65 @@ local M = {}
 ---@field binds Keybinds|nil
 ---@field picker boolean|nil Activate .picker, this requires having Telescope installed
 
+---@type Config
+M.default_config = {
+  binds = {
+    sub_in = "<Leader>s",
+    super_in = "<Leader>S",
+  }
+}
+
 local super_chars = {}
 local sub_chars = {}
 
-for _,c in pairs(require("symbols")) do
-    if c[3] == "superscript" then
-        super_chars[c[2]] = c[1]
-    elseif c[3] == "subscript" then
-        sub_chars[c[2]] = c[1]
-    end
+for _, c in pairs(require("symbols")) do
+  if c[3] == "superscript" then
+    super_chars[c[2]] = c[1]
+  elseif c[3] == "subscript" then
+    sub_chars[c[2]] = c[1]
+  end
 end
 
 ---Search for the equivalent character under the specified type
 ---@param char string Character to match for symbol
 ---@param type Symbol_type Type of symbol list to search
----@return string|nil 
-local function get_symbol(char,type)
-    local list = type == "sub" and sub_chars or super_chars
-    local match_char = list[char]
+---@return string|nil
+local function get_symbol(char, type)
+  local list = type == "sub" and sub_chars or super_chars
+  local match_char = list[char]
 
-    if match_char ~= nil then
-        return match_char
-    end
+  if match_char ~= nil then
+    return match_char
+  end
 
-    print("No match found for: "..char.." ("..(type == "sub" and "subscript" or "superscript")..")")
+  print("No match found for: " .. char .. " (" .. (type == "sub" and "subscript" or "superscript") .. ")")
 end
 
 ---Next keystroke will be used to place the equivalent symbol under the cursor
 ---@param s_type Symbol_type Type of symbol list to search
 local function put_char(s_type)
-    local status,input_char = pcall(vim.fn.getcharstr)
-    if not status then
-        print((s_type == "super" and "Superscript" or "Subscript").." input aborted.")
-        return
-    end
-    local output_char = get_symbol(input_char,s_type)
-    vim.api.nvim_put({output_char},"",true,true)
+  local status, input_char = pcall(vim.fn.getcharstr)
+  if not status then
+    print((s_type == "super" and "Superscript" or "Subscript") .. " input aborted.")
+    return
+  end
+  local output_char = get_symbol(input_char, s_type)
+  vim.api.nvim_put({ output_char }, "", true, true)
 end
 
 -- Change binds for super-/subscript input
 ---@param config Config|nil
 M.setup = function(config)
-    ---@type Keybinds
-    local binds = {
-        super_in = config and config.binds and config.binds.super_in or "<Leader>S",
-        sub_in = config and config.binds and config.binds.sub_in or "<Leader>s",
-    }
+  if not config then config = {} end
 
-    vim.keymap.set("n", binds.super_in, function() put_char("super") end,{noremap=true,silent=true})
-    vim.keymap.set("n", binds.sub_in, function() put_char("sub") end,{noremap=true,silent=true})
+  config = vim.tbl_deep_extend("keep", config, M.default_config)
 
-    if config and config.picker then
-        M.picker = require("picker").get_symbols
-    end
+  vim.keymap.set("n", config.binds.super_in, function() put_char("super") end, { noremap = true, silent = true })
+  vim.keymap.set("n", config.binds.sub_in, function() put_char("sub") end, { noremap = true, silent = true })
+
+  if config.picker then
+    M.picker = require("picker").get_symbols
+  end
 end
 
 return M
